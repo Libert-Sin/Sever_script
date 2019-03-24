6265f72a24699201756a1a4141702c4f3cb4609a
Libert Sin <majesticer13@gmail.com>
2019-03-24 11:09:28 +0000

    Update full_rollback.sh

--- full_back/full_rollback.sh
+++ full_back/full_rollback.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
 echo -e "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
+
+
+ROOT_UID=0
+E_NOTROOT=67
+ 
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+echo "Script have to execute on ROOT mode"
+exit $E_NOTROOT
+fi
+
+
+
 
 tar xvpfz backup*.tgz -C /
 mkdir proc
