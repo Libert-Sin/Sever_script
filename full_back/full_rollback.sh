6c2fc06ad1a4aa96136be859b3483b200b67b04a
Libert Sin <majesticer13@gmail.com>
2019-03-24 10:43:47 +0000

    Add new file

---
+++ full_back/full_rollback.sh
@@ -0,0 +1,7 @@
+echo -e "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
+
+tar xvpfz backup*.tgz -C /
+mkdir proc
+mkdir lost+found
+mkdir mnt
+mkdir sys
