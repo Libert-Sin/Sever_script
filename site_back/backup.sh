181b77b4355f8fb0451efefb3f9ece6f9eb6b252
Libert Sin <majesticer13@gmail.com>
2019-03-24 10:54:32 +0000

    Add new file

---
+++ site_back/backup.sh
@@ -0,0 +1,11 @@
+echo -e "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
+
+DATE=`date +"%Y%m%d%H%M%S"`
+
+mkdir ~/backup
+mkdir ~/backup/bck${DATE}
+
+tar zcvf ~/backup/bck${DATE}/${DATE}_html.tar.gz /var/www
+sudo mysqldump -u유저 -p암호 DB이름 > ~/backup/bck${DATE}/${DATE}_DB이름.sql 
+
+sudo chmod 755 -R ~/backup
