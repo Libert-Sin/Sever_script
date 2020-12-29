e7473829015656db744705afa0f67e6cc102eed0
Libert Sin <libert@photoguraphy.com>
2020-12-29 13:01:16 +0000

    Update site_back/clean.sh

--- site_back/clean.sh
+++ site_back/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "#### 이 프로그램의 저작권은 ⓒLibert Sin 에 있습니다. ####\n 자유로운 배포·수정은 허용하지만 원저작자와 출처를 표시해야 합니다.\n http://photoguraphy.com/\n#############\n\n\n"
 
-rm -rf ~/backup/*
+rm -rf /백업경로/*
